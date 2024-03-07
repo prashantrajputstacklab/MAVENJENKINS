90ca80d0896b11053cb60e9c1528021414dfc37a
prashantrajputstacklab <prashant.rajput@stacklab.in>
2024-03-07 14:35:15 +0530

    Third

--- src/test/java/OrangHrm/LoginPage.java
+++ src/test/java/OrangHrm/LoginPage.java
@@ -16,6 +16,7 @@ public class LoginPage {
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
 		System.out.println("Pass");
+		System.out.println("pass1");
 	}
  
 
